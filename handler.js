f33735a1b3160fbe1dc6d8dbb34922a9badc1991
James Hutcheon <hutcho66@gmail.com>
2019-02-02 23:08:53 +1100

    first attempt, emailing to self

---
+++ handler.js
@@ -0,0 +1,96 @@
+'use strict'
+
+const axios = require('axios');
+const imageDownloader = require('image-downloader');
+const nodemailer = require('nodemailer');
+const fs = require('fs-promise');
+const aws = require('aws-sdk');
+const moment = require('moment');
+const RSSParser = require('rss-parser');
+const htmlParse = require('node-html-parser').parse;
+
+aws.config.update({region: 'us-west-2'});
+
+const feedUrl = 'http://feed.dilbert.com/dilbert/daily_strip';
+const tempPath = '/tmp';
+
+const rssParser = new RSSParser();
+
+const getHtmlPage = async (url) => {
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (err) {
+        console.log(error);
+    }
+}
+
+const getComicLink = (html) => {
+    const root = htmlParse(html);
+
+    return {
+        link: 'https:' + root.querySelector('.img-comic').attributes.src,
+        title: root.querySelector('.comic-title-name').text
+    };
+}
+
+const saveImage = async (imgLink, fname) => {
+    const options = {
+        url: imgLink,
+        dest: `${tempPath}/${fname}.gif`
+    };
+
+    try {
+        if (!fs.existsSync(tempPath)) {
+            fs.mkdirSync(tempPath);
+        }
+        await imageDownloader.image(options);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+const sendImageEmail = async (imgFilename, title) => {
+    const transporter = nodemailer.createTransport({
+        SES: new aws.SES({
+            apiVersion: '2010-12-01'
+        })
+    });
+
+    const mailOptions = {
+        from: 'dilbert@jbhutcheon.com',
+        to: 'James.Hutcheon@team.telstra.com',
+        subject: `Dilbert ${moment(imgFilename, "YYYY-MM-DD").format('DD MMM YYYY')}: ${title}`,
+        html: '<img src="cid:imgFilename"/>',
+        attachments: [{
+            filename: imgFilename + '.gif',
+            path: `${tempPath}/${imgFilename}.gif`,
+            cid: 'imgFilename'
+        }]
+    }
+
+    return new Promise((resolve, reject) => {
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            } else {
+                console.log(info);
+                resolve();
+            }
+        });
+    });
+    
+}
+
+module.exports.getDilbert = async (event, context) => {
+    const feed = await rssParser.parseURL(feedUrl);
+    const item = feed.items[0]; // most recent is at top of list
+
+    const html = await getHtmlPage(item.link);
+    const { link, title } = getComicLink(html);
+    await saveImage(link, item.id);
+    await sendImageEmail(item.id, title);
+
+    console.log('Sent daily dilbert!');
+};
